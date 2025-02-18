77f195d79bb61a40bf09cab3b9d68abf515e41fb
grigorov <grigmak446@gmail.com>
2025-02-18 10:18:54 +0300

    l8

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mkcert from 'vite-plugin-mkcert'
-import fs from 'fs'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   // base: '/RepoName',
   server: {
-    https:{
-      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
-    },
     port: 3000,
     host: true,
     proxy: {
@@ -27,5 +20,5 @@ export default defineConfig({
       }
     }
   },
-  plugins: [react(), mkcert()]
+  plugins: [react()],
 })
